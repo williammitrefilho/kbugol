f7907a91d5c5b97f8e1c8dcf523d5abb6e766c37
William Mitre Filho <willmitre@gmail.com>
2026-02-21 09:55:40 -0300

    Now possible to add and remove listeners and to specify a single target

--- kbugol.js
+++ kbugol.js
@@ -1,43 +1,60 @@
 class KBugol{
+	static sendToAll(eventName){
+		if(!KBugol.listeners[eventName])
+			return
+
+		KBugol.listeners[eventName].forEach((listener)=>{
+			listener[eventName]?.()
+		})
+	}
+	static addListener(eventName, listener){
+		if(!KBugol.listeners[eventName])
+			KBugol.listeners[eventName] = []
+
+		KBugol.listeners[eventName].push(listener)
+	}
+	static removeListener(eventName, listener){
+		if(!KBugol.listeners[eventName])
+			return
+		
+		let listeners = KBugol.listeners[eventName]
+		listeners.splice(listeners.indexOf(listener, 1))
+	}
+
+	send(eventName){
+		if(KBugol.target)
+			KBugol.target[eventName]?.()
+		else
+			KBugol.sendToAll(eventName)
+	}
+
 	constructor(){
 		this.controllered = null
 	}
-	
-	arrowKeyPressed(e){
-	}
-
-	tabKeyPressed(e){
-	}
-
-	enterKeyPressed(e){
-	}
-	
-	letterKeyPressed(e){
-	}
-
-	numberKeyPressed(e){
-	}
-	
 	listen(){
 		this.oldListener = window.onkeydown
 		window.onkeydown = (e) => {
-			this.findListenerFor(e)
+			this.parseListener(e)
 		}
 	}
 	stopListening(){
 		
 		window.onkeydown = this.oldListener
 	}
-	findListenerFor(e){
+	parseListener(e){
 		if(e.target.nodeName.toLowerCase() == "input")
 			return
 
 		e.preventDefault()
-		for(var category in KBugol.codes){
-			let codes = KBugol.codes[category]
-			if(codes.includes(e.code))
-				return this[`${category}KeyPressed`](e)
+		const eventName = `key${e.key.toUpperCase()}Pressed`
+		for(var category in KBugol.keys){
+			let keys = KBugol.keys[category]
+			if(keys.includes(e.key)){
+				this[`${category}KeyPressed`]?.(e)
+				break;
+			}
 		}
+		this.send(eventName)
 	}
 }
 
@@ -63,3 +80,24 @@ KBugol.codes = {
 		"KeyA","KeyB","KeyC","KeyD","KeyE","KeyF","KeyG","KeyH","KeyI","KeyJ","KeyK","KeyL","KeyM","KeyN","KeyO","KeyP","KeyQ","KeyR","KeyS","KeyT","KeyU","KeyV","KeyW","KeyX","KeyY","KeyZ",
 	],
 }
+KBugol.keys = {
+	arrow:[
+		"ArrowUp",
+		"ArrowDown",
+		"ArrowLeft",
+		"ArrowRight",
+	],
+	tab:[
+		"Tab",
+	],
+	enter:[
+		"Enter",
+	],
+	number:[
+		"0","1","2","3","4","5","6","7","8","9",
+	],
+	letter:[
+		"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+	],
+}
+KBugol.listeners = {}
